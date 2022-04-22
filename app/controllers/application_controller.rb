84cc4614737e3b21bc00512114f56b7157e57605
juliannaresende <jresende@chicagobooth.edu>
2022-04-22 16:32:54 +0000

    paginas iniciais

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
 
+  def login
+    render({:template=>"calc_templates/calc_add.html.erb"})
+  end
+  def calc_add
+    render({:template=>"calc_templates/calc_add.html.erb"})
+  end
+  def calc_subtract
+    render({:template=>"calc_templates/calc_subtract.html.erb"})
+  end
+  def calc_multiply
+    render({:template=>"calc_templates/calc_multiply.html.erb"})
+  end  
+  def calc_divide
+  render({:template=>"calc_templates/calc_divide.html.erb"})
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get("/", { :controller => "application", :action => "login" })
+
+  get("/add", { :controller => "application", :action => "calc_add" })
+
+  get("/subtract", { :controller => "application", :action => "calc_subtract" })
+
+  get("/multiply", { :controller => "application", :action => "calc_multiply" })
+
+  get("/divide", { :controller => "application", :action => "calc_divide" })
+
 end
